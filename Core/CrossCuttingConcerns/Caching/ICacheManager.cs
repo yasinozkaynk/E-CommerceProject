421e880335a6373cb4df55808d80d1726224bf83
yasinozkaynk <yasnozkaynk@gmail.com>
2021-03-04 23:45:13 +0300

    .

--- Core/CrossCuttingConcerns/Caching/ICacheManager.cs
+++ Core/CrossCuttingConcerns/Caching/ICacheManager.cs
@@ -6,6 +6,9 @@ namespace Core.CrossCuttingConcerns.Caching
 {
     public interface ICacheManager
     {
+
+
+
         T Get<T>(string key);
         object Get(string key);
         void Add(string key, object value, int durattion);
